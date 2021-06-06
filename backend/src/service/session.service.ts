62803f19d84396f3aca08ad1bc408dc0569c2978
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-06 16:01:54 +0100

    Session Service create access token

--- backend/src/service/session.service.ts
+++ backend/src/service/session.service.ts
@@ -1,9 +1,31 @@
 // * 13 - Session Service
 
-import Session from '../model/session.model'
+import { LeanDocument } from 'mongoose'
+import Session, { SessionDocumentInterface } from '../model/session.model'
+import { UserDocumentInterface } from '../model/user.model'
+import config from 'config'
 
 export async function createSession(userId: string, userAgent: string) {
   const session = await Session.create({ user: userId, userAgent })
 
   return session.toJSON()
+}
+
+export function createAccessToken({
+  user,
+  session
+}: {
+  user:
+    | Omit<UserDocumentInterface, 'password'>
+    | LeanDocument<Omit<UserDocumentInterface, 'password'>>
+  session:
+    | Omit<SessionDocumentInterface, 'password'>
+    | LeanDocument<Omit<SessionDocumentInterface, 'password'>>
+}) {
+  // Build and return the new access token
+  const accesstoken = sign(
+    { ...user, session: session._id },
+    { expiresIn: config.get('accessTokenTtl') }
+  )
+  return accesstoken
 }
