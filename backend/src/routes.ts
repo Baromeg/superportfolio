4f192989d02d223dd2ec000f893f5451885dc106
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-06 13:36:08 +0100

    Plan the routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,12 +4,18 @@ import { Express, Request, Response } from 'express'
 
 export default function (app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+  // Test everthing is running properly by processing ac curl request in the terminal.
+  // curl http://localhost:5002/healthcheck in the console
 
   // Register user
+  // POST /api/user
 
   // Login
+  // POST /api/sessions
 
   // Get the user's sessions
+  // GET /api/sessions
 
   // Logout
+  // DELETE /api/sessions
 }
