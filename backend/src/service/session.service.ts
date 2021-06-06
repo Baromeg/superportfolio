c9adeb4ec27d27a7d9d21efa3573974c4fb6631b
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-06 15:47:44 +0100

    13 - Session Service

---
+++ backend/src/service/session.service.ts
@@ -0,0 +1,9 @@
+// * 13 - Session Service
+
+import Session from '../model/session.model'
+
+export async function createSession(userId: string, userAgent: string) {
+  const session = await Session.create({ user: userId, userAgent })
+
+  return session.toJSON()
+}
