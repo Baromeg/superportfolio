82ecc88b71858ef1f5eb109b0a57b829fbd0f8a9
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-06 14:16:17 +0100

    10 - UserSchema and Validate Request

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,9 @@
 // * 5 - Create the routes
 
 import { Express, Request, Response } from 'express'
+import { createUserHandler } from './controller/user.controller'
+import validateRequest from './middleware/validateRequest'
+import { createUserSchema } from './schema/user.schema'
 
 export default function (app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
@@ -9,6 +12,7 @@ export default function (app: Express) {
 
   // Register user
   // POST /api/user
+  app.post('/api/users', validateRequest(createUserSchema), createUserHandler)
 
   // Login
   // POST /api/sessions
