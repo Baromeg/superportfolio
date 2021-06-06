1d5aca8212e099498595ddcb4944c82d389762fe
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-06 15:12:20 +0100

    11 - Create the Session Model

---
+++ backend/src/model/session.model.ts
@@ -0,0 +1,31 @@
+// * 11 - Create the Session model
+
+import mongoose from 'mongoose'
+import { UserDocumentInterface } from './user.model'
+
+// Interface
+export interface SessionDocumentInterface extends mongoose.Document {
+  email: UserDocumentInterface['_id']
+  valid: boolean
+  userAgent: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+// User Schema
+const SessionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String }
+  },
+  { timestamps: true }
+)
+
+// User Model
+const Session = mongoose.model<SessionDocumentInterface>(
+  'Session',
+  SessionSchema
+)
+
+export default Session
